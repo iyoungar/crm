57bc63703aa76e95c550280a2c52951b00c43489
iyoungar <iyoungarn@gmail.com>
2020-12-23 21:00:17 +0100

    backend structure include

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,15 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/', views.customer),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.LoginPage, name='login'),
+
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk_test>', views.customer, name='customer'),
+
+    path('create_order/<str:pk>/', views.create_order, name='create_order'),
+    path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
+    path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order')
+
 ]
